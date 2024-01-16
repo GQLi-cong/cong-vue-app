9074984c10bcae78f0ed576e1c7a54662a66546e
GQLi-cong <2571875806@qq.com>
2024-01-16 23:25:04 +0800

    修改0116

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
+import {Row, Button} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(Row)
+Vue.use(Button)
 
 new Vue({
   render: h => h(App),
