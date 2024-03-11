fb4925a6d6d9c8de3c6b780aafbd31561e0179e9
GQLi-cong <2571875806@qq.com>
2024-03-11 23:28:36 +0800

    新建左侧菜单栏

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
-        <h1>Main</h1>
-        <router-view></router-view>
+        <el-container>
+            <el-aside width="200px">
+                <common-aside />
+            </el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 <script>
+import CommonAside from '../components/CommonAside.vue'
 export default {
     data() {
         return {}
+    },
+    components: {
+        CommonAside
     }
 }
 </script>
