39de3e714ab9d9b6f86cc7ceb6ead58538aefdcb
GQLi-cong <2571875806@qq.com>
2024-03-27 23:02:11 +0800

    home订单统计部分

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,14 +14,131 @@
                     <p>上次登录地点：<span>丁家庄</span></p>
                 </div>
             </el-card>
+            <el-card style="margin-top: 20px; height: 460px;">
+                <el-table
+                :data="tableData"
+                style="width: 100%">
+                    <!-- <el-table-column
+                        prop="name"
+                        label="课程">
+                    </el-table-column>
+                    <el-table-column
+                        prop="todayBuy"
+                        label="今日购买">
+                    </el-table-column>
+                    <el-table-column
+                        prop="monthBuy"
+                        label="本月购买">
+                    </el-table-column>
+                    <el-table-column
+                        prop="totalBuy"
+                        label="总购买">
+                    </el-table-column> -->
+                    <el-table-column v-for="(val, key) in tableLabel" :prop="key" :label="val" />
+                </el-table>
+            </el-card>
         </el-col>
-        <el-col :span="16"><div class="grid-content bg-purple-light"></div></el-col>
+        <el-col :span="16">
+            <div class="num">
+                <el-card v-for="item in countData" :key="item.name" :body-style="{ display: 'flex', padding: 0}">
+                    <i class="icon" :class="`el-icon-${item.icon}`" :style="{ background: item.color }"></i>
+                    <div class="detail">
+                        <p class="price"> ￥{{ item.value }}</p>
+                        <p class="desc">{{ item.name }}</p>
+                    </div>
+                </el-card>
+            </div>
+        </el-col>
     </el-row>
 </template>
 <script>
 export default {
     data() {
-        return {}
+        return {
+            tableData: [
+                {
+                    name: '三星',
+                    todayBuy: 300,
+                    monthBuy: 1200,
+                    totalBuy: 23200
+                },
+                {
+                    name: '苹果',
+                    todayBuy: 450,
+                    monthBuy: 1500,
+                    totalBuy: 33200
+                },
+                {
+                    name: '小米',
+                    todayBuy: 150,
+                    monthBuy: 900,
+                    totalBuy: 10100
+                },
+                {
+                    name: '华为',
+                    todayBuy: 230,
+                    monthBuy: 1100,
+                    totalBuy: 16500
+                },
+                {
+                    name: 'OPPO',
+                    todayBuy: 440,
+                    monthBuy: 2100,
+                    totalBuy: 65500
+                },
+                {
+                    name: 'VIVO',
+                    todayBuy: 480,
+                    monthBuy: 1800,
+                    totalBuy: 45500
+                }
+            ],
+            tableLabel: {
+                name: '课程',
+                todayBuy: '今日购买',
+                monthBuy: '本月购买',
+                totalBuy: '总购买',
+            },
+            countData: [
+                {
+                    name: "今日支付订单",
+                    value: 2333,
+                    icon: "success",
+                    color: '#2ec7c9'
+                },
+                {
+                    name: "今日收藏订单",
+                    value: 1356,
+                    icon: "star-on",
+                    color: '#ffb980'
+                },
+                {
+                    name: "今日未支付订单",
+                    value: 1233,
+                    icon: "s-goods",
+                    color: '#5ab1ef'
+                },
+                {
+                    name: "本月支付订单",
+                    value: 633,
+                    icon: "success",
+                    color: '#2ec7c9'
+                },
+                {
+                    name: "本月收藏订单",
+                    value: 423,
+                    icon: "star-on",
+                    color: '#ffb980'
+                },
+                {
+                    name: "本月未支付订单",
+                    value: 666,
+                    icon: "s-goods",
+                    color: '#5ab1ef'
+                },
+            ]
+
+        }
     }
 }
 </script>
@@ -57,6 +174,40 @@ export default {
             color: #666666;
             margin-left: 60px;
         }
+    }
+}
+.num {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    .icon {
+        width: 80px;
+        height: 80px;
+        font-size: 30px;
+        text-align: center;
+        line-height: 80px;
+        color: #fff;
+    }
+    .detail {
+        margin-left: 15px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        .price {
+            font-size: 30px;
+            margin-bottom: 10px;
+            line-height: 30px;
+            height: 30px;
+        }
+        .desc {
+            font-size: 14px;
+            color: #999;
+            text-align: center;
+        }
+    }
+    .el-card {
+        width: 32%;
+        margin-bottom: 20px;
     }
 }
 </style>
